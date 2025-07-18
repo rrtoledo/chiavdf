a8054302d018f5af2dd8570f018a68242524842d
Raphael Toledo <raphael.toledo@iohk.io>
2025-07-18 11:36:38 +0100

    Adding python bindings and benches

--- src/python_bindings/fastvdf.cpp
+++ src/python_bindings/fastvdf.cpp
@@ -23,6 +23,69 @@ PYBIND11_MODULE(chiavdf, m) {
         return D.to_string();
     });
 
+
+    // Generate an int of certain length
+    m.def("hash_int", [] (const py::bytes& challenge_hash, int int_length) {
+        std::string challenge_hash_str(challenge_hash);
+        integer p;
+        {
+            py::gil_scoped_release release;
+            auto challenge_hash_bits = std::vector<uint8_t>(challenge_hash_str.begin(), challenge_hash_str.end());
+            p = HashInt(challenge_hash_bits, int_length);
+        }
+        return p.to_string();
+    });
+
+
+    // Generate a prime of certain length
+    m.def("hash_prime", [] (const py::bytes& challenge_hash, int prime_length) {
+        std::string challenge_hash_str(challenge_hash);
+        integer p;
+        {
+            py::gil_scoped_release release;
+            auto challenge_hash_bits = std::vector<uint8_t>(challenge_hash_str.begin(), challenge_hash_str.end());
+            p = HashPrime(challenge_hash_bits, prime_length, {prime_length-1});
+        }
+        return p.to_string();
+    });
+
+    // Checks a simple wesolowski proof.
+    m.def("hash_prime_both", [] (const string& discriminant,
+                                   const string& x_s, const string& y_s) {
+        integer D(discriminant);
+        std::string x_s_copy(x_s);
+        std::string y_s_copy(y_s);
+        integer p;
+        {
+            py::gil_scoped_release release;
+            form x = DeserializeForm(D, (const uint8_t *)x_s_copy.data(), x_s_copy.size());
+            form y = DeserializeForm(D, (const uint8_t *)y_s_copy.data(), y_s_copy.size());
+            p = GetB(D, x, y);
+        }
+        return p.to_string();
+    });
+
+   // Checks a simple wesolowski proof.
+    m.def("hash_int_both", [] (const string& discriminant,
+                                   const string& x_s, const string& y_s, int int_length) {
+        integer D(discriminant);
+        std::string x_s_copy(x_s);
+        std::string y_s_copy(y_s);
+        integer p;
+        {
+            py::gil_scoped_release release;
+            form x = DeserializeForm(D, (const uint8_t *)x_s_copy.data(), x_s_copy.size());
+            form y = DeserializeForm(D, (const uint8_t *)y_s_copy.data(), y_s_copy.size());
+            int d_bits = D.num_bits();
+            std::vector<unsigned char> serialization = SerializeForm(x, d_bits);
+            std::vector<unsigned char> serialization_y = SerializeForm(y, d_bits);
+            serialization.insert(serialization.end(), serialization_y.begin(), serialization_y.end());
+            p = HashInt(serialization, int_length);
+        }
+        return p.to_string();
+    });
+
+
     // Checks a simple wesolowski proof.
     m.def("verify_wesolowski", [] (const string& discriminant,
                                    const string& x_s, const string& y_s,
@@ -94,6 +157,73 @@ PYBIND11_MODULE(chiavdf, m) {
             );
             form x = DeserializeForm(D, (const uint8_t *) x_s_copy.data(), x_s_copy.size());
             result = ProveSlow(D, x, num_iterations, shutdown_file_path_copy);
+        }
+        py::bytes ret = py::bytes(reinterpret_cast<char*>(result.data()), result.size());
+        return ret;
+    });
+
+    m.def("prove_disc", [] (const string& discriminant, const string& x_s, uint64_t num_iterations, const string& shutdown_file_path) {
+        integer D(discriminant);
+        std::string x_s_copy(x_s);
+        std::vector<uint8_t> result;
+        std::string shutdown_file_path_copy(shutdown_file_path);
+        {
+            py::gil_scoped_release release;
+            form x = DeserializeForm(D, (const uint8_t *) x_s_copy.data(), x_s_copy.size());
+            result = ProveSlow(D, x, num_iterations, shutdown_file_path_copy);
+        }
+        py::bytes ret = py::bytes(reinterpret_cast<char*>(result.data()), result.size());
+        return ret;
+    });
+
+    m.def("evaluate", [] (const string& discriminant, const string& x_s, uint64_t num_iterations, const string& shutdown_file_path) {
+        integer D(discriminant);
+        std::string x_s_copy(x_s);
+        std::vector<uint8_t> result;
+        std::string shutdown_file_path_copy(shutdown_file_path);
+        {
+            py::gil_scoped_release release;
+            form x = DeserializeForm(D, (const uint8_t *) x_s_copy.data(), x_s_copy.size());
+            result = EvaluateOnly(D, x, num_iterations, shutdown_file_path_copy);
+        }
+        py::bytes ret = py::bytes(reinterpret_cast<char*>(result.data()), result.size());
+        return ret;
+    });
+
+    m.def("evaluate_slow", [] (const string& discriminant, const string& x_s, uint64_t num_iterations, const string& shutdown_file_path) {
+        integer D(discriminant);
+        std::string x_s_copy(x_s);
+        std::vector<uint8_t> result;
+        std::string shutdown_file_path_copy(shutdown_file_path);
+        {
+            py::gil_scoped_release release;
+            form x = DeserializeForm(D, (const uint8_t *) x_s_copy.data(), x_s_copy.size());
+            result = EvalSlow(D, x, num_iterations, shutdown_file_path_copy);
+        }
+        py::bytes ret = py::bytes(reinterpret_cast<char*>(result.data()), result.size());
+        return ret;
+    });
+
+    m.def("prove_inter", [] (const string& discriminant, const string& x_s, const string& y_s, const string& inter, uint64_t num_iterations) {
+        integer D(discriminant);
+        std::string x_s_copy(x_s);
+        std::string y_s_copy(y_s);
+        size_t l = x_s_copy.length();
+        std::string inter_s_copy(inter);
+        std::vector<form> intermediates;
+        std::vector<uint8_t> result;
+        {
+            py::gil_scoped_release release;
+            form x = DeserializeForm(D, (const uint8_t *) x_s_copy.data(), x_s_copy.size());
+            form y = DeserializeForm(D, (const uint8_t *) y_s_copy.data(), y_s_copy.size());
+            assert(inter_s_copy.length % l == 0);
+            size_t nb = inter_s_copy.length() / l;
+            for (int i = 0; i < nb; i++) {
+                std::string in_s = inter_s_copy.substr(i*l, l);
+                form in = DeserializeForm(D, (const uint8_t *) in_s.data(), in_s.size());
+                intermediates.push_back(in);
+            }
+            result = ProveInter(D,  x,  y, intermediates, num_iterations);
         }
         py::bytes ret = py::bytes(reinterpret_cast<char*>(result.data()), result.size());
         return ret;
